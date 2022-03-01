6e45fbf2f9e82df16ad88110e8022c255111fbce
KhanhHoang <hoangbaokhanh0509@gmail.com>
2022-03-01 20:52:18 +0700

    first commit

---
+++ src/main/HelloWorld.java
@@ -0,0 +1,17 @@
+package main;
+
+public class HelloWorld {
+	//ten pakage: viet thuong
+	//ten class: viet hoa chu dau
+	//Format line -> ctrl + shift + F
+	//Xoa 1 dong -> ctrl + d
+	//tu dong import, xoa import ko can thiet -> ctrl + shift + 0
+	//Save all file -> ctrl + shift + s
+	//Goi y ham, bien -> ctrl + space
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
