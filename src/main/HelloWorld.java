2eb2c9673082862b8f95917258d3efbe18a1a9d4
KhanhHoang <hoangbaokhanh0509@gmail.com>
2022-03-01 21:11:16 +0700

    03/01/2022: Print my name

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -1,17 +1,30 @@
 package main;
 
 public class HelloWorld {
-	//ten pakage: viet thuong
-	//ten class: viet hoa chu dau
-	//Format line -> ctrl + shift + F
-	//Xoa 1 dong -> ctrl + d
-	//tu dong import, xoa import ko can thiet -> ctrl + shift + 0
-	//Save all file -> ctrl + shift + s
-	//Goi y ham, bien -> ctrl + space
+	// ten pakage: viet thuong
+	// ten class: viet hoa chu dau
+	// Format line -> ctrl + shift + F
+	// Xoa 1 dong -> ctrl + d
+	// tu dong import, xoa import ko can thiet -> ctrl + shift + 0
+	// Save all file -> ctrl + shift + s
+	// Goi y ham, bien -> ctrl + space
+	
+	//câu lệnh git
+	//git init: khởi tạo (làm 1 lần)
+	//git add . :lưu file vào git 
+	//git config --global user.email "nhập email"
+	//git config --global user.name "đặt tên"
+	//git commit -m "first commit"
+	//git branch -M master
+	//git remote add origin https://github.com/KhanhHoang0509/day1.git
+	//git push -u origin master
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		System.out.println("My name is Khanh");
 	}
 
 }
